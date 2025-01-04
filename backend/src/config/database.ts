430be17bb9beef8afac007b0dd2a29ed414be49a
Tuomas <tuomas@jmellin.net>
2025-01-04 20:34:32 +0200

    Updated the way sequelize connects to the database and handles the cli operations (Migrations, Models and Seeds)

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,12 +1,25 @@
-import { sequelize } from '../models'
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const connectDB = async (): Promise<Boolean> => {
     try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        const databaseConnection = new Sequelize({
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_DATABASE,
+            host:  process.env.DATABASE_HOST,
+            dialect: 'mariadb',
+        });
+
+        await databaseConnection.authenticate()
+        console.log('Connected to the database successfully.');
+
         return true;
     } catch (error) {
         console.error('Unable to connect to the database:', error);
+
         return false;
     }
 }
