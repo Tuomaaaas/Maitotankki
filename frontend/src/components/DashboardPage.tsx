fd67760bdd5bf4c81faf4e3153d99470d5eb2dc4
Tuomas <tuomas@jmellin.net>
2025-01-05 01:13:05 +0200

    Initialize frontend

---
+++ frontend/src/components/DashboardPage.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+
+    return (
+        <div className="dashboard-container">
+            <h1>Welcome to the Dashboard!</h1>
+        </div>
+    );
+}
+
+export default DashboardPage
