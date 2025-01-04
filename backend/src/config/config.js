6d58d15293fa8f53c42551f3c105b19e6a52c541
Tuomas Mellin <120010656+Tuomaaaas@users.noreply.github.com>
2025-01-04 20:36:33 +0200

    Updated the way sequelize connects to the database and handles the cli operations (Migrations, Models and Seeds) (#7)
    Squash and merge.

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,26 +1,11 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 module.exports = {
-    development: {
-        username: process.env.DEVELOPMENT_USERNAME,
-        password: process.env.DEVELOPMENT_PASSWORD,
-        database: process.env.DEVELOPMENT_DATABASE,
-        host: process.env.DEVELOPMENT_HOST,
-        dialect: 'mariadb',
-    },
-    staging: {
-        username: process.env.STAGING_USERNAME,
-        password: process.env.STAGING_PASSWORD,
-        database: process.env.STAGING_DATABASE,
-        host: process.env.STAGING_HOST,
-        dialect: 'mariadb',
-    },
-    production: {
-        username: process.env.PRODUCTION_USERNAME,
-        password: process.env.PRODUCTION_PASSWORD,
-        database: process.env.PRODUCTION_DATABASE,
-        host: process.env.PRODUCTION_HOST,
-        dialect: 'mariadb',
-    },
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    host: process.env.DATABASE_HOST,
+    dialect: 'mariadb',
 };
